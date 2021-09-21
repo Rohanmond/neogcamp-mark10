9175eafbffc464c91d4018300505fb677d09d650
Rohan Mondal <mondalrohan108@gmail.com>
2021-09-21 23:31:47 +0530

    update script

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ const billAmount=document.querySelector("#bill-amount");
 const checkButton=document.querySelector("#check-button");
 const cashGiven=document.querySelector("#cash-given");
 const message=document.querySelector("#error-message");
+const noOfNotes=document.querySelectorAll(".numberOfNotes");
+const availableNotes=[2000,500,200,100,50,20,10,5,2,1];
 
 checkButton.addEventListener("click",function validateBillAmountAndCashAmount(){
     message.style.display="none";
     if(billAmount.value>0){
         if(cashGiven.value>=billAmount.value){
-            const amountToBeReturned=cashGiven-billAmount;
+            const amountToBeReturned=cashGiven.value-billAmount.value;
             calculateChange(amountToBeReturned);
-
         }else{
             showMessage(message,"Cash given is lesser than the bill amount")
         }
@@ -17,29 +18,15 @@ checkButton.addEventListener("click",function validateBillAmountAndCashAmount(){
        showMessage(message,"Invalid Bill amount");
     }
 });
-function binarySearchIndex (array, target) {
-    var low=0,high=array.length;
-    var ans;
-    while(low<=high){
-        var mid=(low+high)/2;
-        if(target>=array[mid]){
-            ans=mid;
-            low=mid+1;
-        } else{
-            high=mid-1;
-        }
-    }
-    return ans;
-  }
+
 function calculateChange(amountToBeReturned){
-    const cashArray=[1,2,5,10,20,50,100,200,500,2000];
-    while(amountToBeReturned!=0){
-        var idx=binarySearchIndex(cashArray,amountToBeReturned);
-        if(idx!=0) idx=idx-1;
-        console.log(cashArray[idx]);
-        amountToBeReturned=amountToBeReturned-cashArray[idx];
+    for(var i=0;i<availableNotes.length;i++){
+        var noNotes=Math.trunc(amountToBeReturned/availableNotes[i]);
+        amountToBeReturned=amountToBeReturned%availableNotes[i];
+        noOfNotes[i].innerText=noNotes;
     }
 }
+
 function showMessage(message,messageText){
     message.style.display="block";
     message.innerText=messageText;
