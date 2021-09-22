f59d2d2a223ed02d3380b331b8be41cc5dc92578
Rohan Mondal <mondalrohan108@gmail.com>
2021-09-22 11:13:22 +0530

    update script.js

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ const noOfNotes=document.querySelectorAll(".numberOfNotes");
 const availableNotes=[2000,500,200,100,50,20,10,5,2,1];
 
 checkButton.addEventListener("click",function validateBillAmountAndCashAmount(){
+    console.log(cashGiven.value,billAmount.value)
+    var cash=parseInt(cashGiven.value);
+    var bill=parseInt(billAmount.value);
+
     message.style.display="none";
     if(billAmount.value>0){
-        if(cashGiven.value>=billAmount.value){
-            const amountToBeReturned=cashGiven.value-billAmount.value;
+        if(cash>=bill){
+            const amountToBeReturned=cash-bill;
             calculateChange(amountToBeReturned);
         }else{
             showMessage(message,"Cash given is lesser than the bill amount")
